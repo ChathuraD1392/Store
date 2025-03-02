41ce729a976388fe585cc0c50417bb0a28b1fc85
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-03-02 10:32:58 +0530

    connect to db with prisma and creates header

---
+++ app/components/layouts/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AnnouncementBar from "./AnnouncementBar";
+
+const Header = () => {
+  return (
+    <>
+      <AnnouncementBar />
+      <div>Header</div>
+    </>
+  );
+};
+
+export default Header;
