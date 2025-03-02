2812d917bc1a733c48d3bc162e2cf040a31bd404
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-03-02 12:53:40 +0530

    create announcement bar

--- app/components/layouts/AnnouncementBar.tsx
+++ app/components/layouts/AnnouncementBar.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const AnnouncementBar = () => {
-  return <div>AnnouncementBar</div>;
+  return (
+    <div className="w-full bg-blue-400 py-2">
+      <div className="container mx-auto flex items-center justify-center px-8">
+        <span className="text-center text-sm font-medium tracking-wide text-white">
+          FREE SHIPPING ON ORDERS OVER $50.00 & FREE RETURNS
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default AnnouncementBar;
--- app/components/layouts/Header.tsx
+++ app/components/layouts/Header.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import AnnouncementBar from "./AnnouncementBar";
 
 const Header = () => {
+  const [isVisible, setVisibility] = useState<boolean>(true);
+  const [prevScrollY, setPrevScrollY] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY < prevScrollY) {
+        setVisibility(true);
+      } else if (currentScrollY > 100) {
+        setVisibility(false);
+      }
+      setPrevScrollY(currentScrollY);
+    };
+    setPrevScrollY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollY]);
   return (
     <>
-      <AnnouncementBar />
-      <div>Header</div>
+      <header className="w-full sticky top-0 z-50">
+        <div
+          className={`w-full transform transition-transform duration-400 ease-in-out ${
+            isVisible ? "translate-y-0" : "-translate-y-full"
+          }`}
+        >
+          <AnnouncementBar />
+        </div>
+      </header>
     </>
   );
 };
